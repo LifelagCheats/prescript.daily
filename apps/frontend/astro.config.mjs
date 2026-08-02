a0b8ddc74ed3968a6c392c9cdfdee1e2e56950f1
LifelagCheats <179240443+LifelagCheats@users.noreply.github.com>
2026-08-02 11:10:45 -0400

    chore(tooling): enable ngrok testing in astro config file

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -4,4 +4,9 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  vite: {
+    server: {
+      allowedHosts: ['.ngrok-free.app'],
+    },
+  },
 });
