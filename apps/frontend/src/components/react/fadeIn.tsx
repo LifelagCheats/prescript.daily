097c574e884cad58b5c6e25624f8503dfac892af
LifelagCheats <179240443+LifelagCheats@users.noreply.github.com>
2026-08-07 12:07:43 -0400

    chore(react): add a Fade In modular component, remember to hydrate

---
+++ apps/frontend/src/components/react/fadeIn.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+type Props = {
+  children: React.ReactNode;
+  delay?: number;
+};
+
+export default function FadeIn({ children, delay = 0 }: Props) {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(true);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [delay]);
+
+  return <div className={`FadeIn ${visible ? 'show' : ''}`}>{children}</div>;
+}
