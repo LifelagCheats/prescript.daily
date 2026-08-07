4af88c80f0a3e5a22d64efa0fb7a08ef09897a60
LifelagCheats <179240443+LifelagCheats@users.noreply.github.com>
2026-08-07 12:33:19 -0400

    feat(ui): add the placeholder Streak system, later to be fully implemented

---
+++ apps/frontend/src/components/react/streak.tsx
@@ -0,0 +1,19 @@
+import CountUp from '@components/react/counter';
+import FadeIn from '@components/react/fadeIn';
+
+type Props = {
+  streak: number;
+};
+
+export default function StreakCounter({ streak }: Props) {
+  return (
+    <FadeIn>
+      <span className="Streak IndexText" id="Streak">
+        Streak{' '}
+        <span className="Counter">
+          <CountUp from={0} to={streak} separator="," direction="up" />
+        </span>
+      </span>
+    </FadeIn>
+  );
+}
