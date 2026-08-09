6152ea7f023c4d48aebcc2c73214d264ec6a2215
LifelagCheats <179240443+LifelagCheats@users.noreply.github.com>
2026-08-09 17:56:23 -0400

    refactor(style): move fade in component to be universal across the entire page

--- apps/frontend/src/components/react/streak.tsx
+++ apps/frontend/src/components/react/streak.tsx
@@ -1,5 +1,4 @@
 import CountUp from '@components/react/counter';
-import FadeIn from '@components/react/fadeIn';
 
 type Props = {
   streak: number;
@@ -7,13 +6,11 @@ type Props = {
 
 export default function StreakCounter({ streak }: Props) {
   return (
-    <FadeIn>
-      <span className="Streak" id="Streak">
-        Streak{' '}
-        <span className="Counter">
-          <CountUp from={0} to={streak} separator="," direction="up" />
-        </span>
+    <span className="Streak" id="Streak">
+      Streak{' '}
+      <span className="Counter">
+        <CountUp from={0} to={streak} separator="," direction="up" />
       </span>
-    </FadeIn>
+    </span>
   );
 }
